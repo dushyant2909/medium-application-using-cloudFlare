2cd2e69a4d590091217f77da4b8e01b07a73fe26
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-06-16 00:11:06 +0530

    Added frontend to project

--- common/src/index.ts
+++ common/src/index.ts
@@ -2,29 +2,29 @@ import z from "zod";
 
 export const signupInput = z.object({
   email: z.string().email(),
-  password: z.string().min(4),
+  password: z.string().min(6),
   name: z.string().optional(),
 });
 
+export type SignupInput = z.infer<typeof signupInput>;
+
 export const signinInput = z.object({
   email: z.string().email(),
-  password: z.string().min(4),
+  password: z.string().min(6),
 });
 
-export const createBlogPostInput = z.object({
+export type SigninInput = z.infer<typeof signinInput>;
+
+export const createBlogInput = z.object({
   title: z.string(),
   content: z.string(),
 });
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
 
 export const updateBlogInput = z.object({
   title: z.string().optional(),
   content: z.string().optional(),
-  id: z.string(),
+  id: z.number(),
 });
 
-// Above are variables used be backend
-// Below are variables used by frontend
-export type SignupInput = z.infer<typeof signupInput>;
-export type SigninInput = z.infer<typeof signinInput>;
-export type CreateBlogPostInput = z.infer<typeof createBlogPostInput>;
 export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
