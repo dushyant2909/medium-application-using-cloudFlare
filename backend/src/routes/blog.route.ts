e125af134b6f0d8afee96ec3fae9911a4f67f5a6
Dushyant Bhutyani <dushyantb2003@gmail.com>
2024-09-25 21:03:08 +0530

    Remove unwanted import

--- backend/src/routes/blog.route.ts
+++ backend/src/routes/blog.route.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
